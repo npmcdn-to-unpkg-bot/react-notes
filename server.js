a9926f139b2be1faf6ae115ea62452c0d860d85e
Matthew Cale <mattc41190@gmail.com>
2016-08-09 08:38:28 -0500

    more stuffs

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const Hapi = require('hapi');
 const inert = require('inert');
 const server = new Hapi.Server();
-const port = 3000
+const port = 3000;
+const NOTES_DB = path.join(__dirname, 'noteDB.json')
 
 server.connection({ port: port });
 server.register(inert, (err) => {
@@ -13,11 +16,25 @@ server.register(inert, (err) => {
 
   server.route({
     method: 'GET',
+    path: '/api/notes',
+    handler: (request, reply) => {
+      fs.readFile(NOTES_DB, (err, data) => {
+        if (err) {
+          console.error('Oh shit!');
+        }
+        reply(data)
+      })
+    }
+  });
+
+  server.route({
+    method: 'GET',
     path: '/',
     handler: (request, reply) => {
       reply.file('./index.html');
     }
   });
+
 });
 
 
